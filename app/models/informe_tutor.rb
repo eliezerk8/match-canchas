ab66e46def1bedc688fdf10e3574fba8c9274731
sebcisternas <sebcisternas@gmail.com>
2018-12-08 16:22:22 -0300

    Prueba

--- app/models/informe_tutor.rb
+++ app/models/informe_tutor.rb
@@ -1,5 +1,5 @@
 class InformeTutor < ApplicationRecord
-    validates :fecha, presence:true, uniqueness:true
+   
     validates :objetivo, presence:true
     validates :observaciones, presence:true
 
