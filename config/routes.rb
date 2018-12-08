6a2fc703b8bb65aece4744097912093ad330e46c
eliezerk8 <gerson.peralta@gmail.com>
2018-12-08 03:26:42 -0300

    Se crea vista principal, ningun enlace funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
-
-  get 'welcome/menu'
-  root 'welcome#menu'
+  root to: 'page#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
