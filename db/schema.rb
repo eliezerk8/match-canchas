0bbd2390101d36be0664d47bb36ffce10efbe22f
eliezerk8 <gerson.peralta@gmail.com>
2018-12-10 16:38:15 -0300

    se arreglan las vistas, botones sin funcionar

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2018_12_10_182354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "visits_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "carreras", force: :cascade do |t|
     t.string "nombrecarrera"
     t.string "codigo"
