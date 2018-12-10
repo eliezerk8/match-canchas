0022fcd287f74318b6b093fd03a91257b327fd75
sebcisternas <sebcisternas@gmail.com>
2018-12-10 13:00:21 -0300

    Controlador y Vista Facultad

--- app/controllers/facultads_controller.rb
+++ app/controllers/facultads_controller.rb
@@ -1,11 +1,21 @@
 class FacultadsController < ApplicationController
-    def index
-    @facultades=Facultad.all
-    end
+  def index
+  @facultades=Facultad.all
+  end
 
-    def show
-     @facultad= Facultad.find(params[:id])
-    end 
+  def show
+   @facultad= Facultad.find(params[:id])
+  end 
+
+  def new
+
+   @facultad= Facultad.new
 
   end
-  
+
+  def create
+    @facultad= Facultad.new(nombrefacultad: params[:facultad][:nombrefacultad])
+    @facultad.save
+    redirect_to @facultad
+  end
+end
