05b3c79c56b1d48c8dfa8ea982e69af9a13bd548
arv0297 <arv0297@gmail.com>
2018-12-26 11:14:58 -0300

    Modelo Supervisar con Tabla

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_26_045418) do
+ActiveRecord::Schema.define(version: 2018_12_26_140512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,8 @@ ActiveRecord::Schema.define(version: 2018_12_26_045418) do
   end
 
   create_table "supervisars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "estudiante_id"
     t.index ["estudiante_id"], name: "index_supervisars_on_estudiante_id"
