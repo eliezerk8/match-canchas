28b5899feda13df31c5089e2caf3149d8a35762f
sebcisternas <sebcisternas@gmail.com>
2019-01-02 15:33:59 -0300

    Crud completo - Facultad y Carreras

--- app/controllers/carreras_controller.rb
+++ app/controllers/carreras_controller.rb
@@ -1,4 +1,5 @@
 class CarrerasController < ApplicationController
+  before_action :set_carrera, only: [:show,:delete,:edit,:update]
   layout "prueba"
   def index
     @todocarrera= Carrera.all
@@ -10,27 +11,55 @@ class CarrerasController < ApplicationController
     @carrera= Carrera.find(params[:id])
   end
 
+  def delete
+    #@user.destroy
+    
+    @carrera.estado = false
+    @carrera.save
+      respond_to do |format|
+        format.html {redirect_to carreras_path, notice: 'Carrera no existente'}
+      end
+  
+  end
+
+  def update
+      if @carrera.update(carrera_params)
+        respond_to do |format|
+        format.html {redirect_to carrera_path(@carrera), notice: 'Se Actualizaron los datos'}
+      end
+      else
+        render :edit
+      end
+  end
+
+  def edit
+  end
+   
+
   def new
-
-    @carrera= Carrera.new
-
+   @carreras= Carrera.new
   end
 
   def create
     @carrera= Carrera.new(carrera_params)
     if @carrera.save
       redirect_to carreras_path
-  else
+    else
     render 'new'
-      end
+    end
   end
 
 
 
 
-end
 
-private
-def carrera_params
+ private
+  def carrera_params
   params.require(:carrera).permit(:nombrecarrera, :codigo, :facultad_id, :estado)
+  end
+
+  def set_carrera
+    @carrera = Carrera.find(params[:id])
+  end
+
 end
--- app/controllers/facultads_controller.rb
+++ app/controllers/facultads_controller.rb
@@ -1,5 +1,7 @@
 class FacultadsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_facultad, only: [:show,:delete,:edit,:update]
+
   layout "prueba"
   def index
     @facultades=Facultad.all
@@ -9,6 +11,31 @@ class FacultadsController < ApplicationController
   def show
     @facultad= Facultad.find(params[:id])
   end 
+  
+  def delete
+    #@user.destroy
+    
+    @facultad.estado = false
+    @facultad.save
+      respond_to do |format|
+        format.html {redirect_to facultads_path, notice: 'Facultad no existente'}
+      end
+  
+  end
+
+  def update
+      if @facultad.update(facultad_params)
+        respond_to do |format|
+        format.html {redirect_to facultad_path(@facultad), notice: 'Se Actualizaron los datos'}
+      end
+      else
+        render :edit
+      end
+  end
+
+  def edit
+  end
+   
 
   def new
    @facultads= Facultad.new
@@ -23,8 +50,15 @@ class FacultadsController < ApplicationController
     end
   end
   
-end
+
   private
     def facultad_params
       params.require(:facultad).permit(:nombrefacultad, :estado)
     end
+
+    def set_facultad
+      @facultad = Facultad.find(params[:id])
+    end
+
+
+  end
